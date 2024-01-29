f69cf11250ca586c02cdfb8250b48f0982466c32
Gagan Tuli <gagan1825.cse18@chitkara.edu.in>
2024-01-28 21:28:39 -0500

    Update trainable_layers.py

--- trainable_layers.py
+++ trainable_layers.py
@@ -3,7 +3,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import tensorflow as tf
 
-def define_dense_model_with_hidden_layers(input_length, activation_func_array=['sigmoid','sigmoid'],
+def define_dense_model_with_hidden_layers(input_length, activation_func_array=['sigmoid', 'sigmoid'],
                                           hidden_layers_sizes=[50, 20], output_function='softmax', output_length=10):
     model = keras.Sequential()
     model.add(layers.InputLayer(input_shape=(input_length,)))
@@ -17,17 +17,18 @@ def set_layers_to_trainable(model, trainable_layer_numbers):
         layer.trainable = i in trainable_layer_numbers
     return model
 
-# Intentional errors to make the code fail
+# Intentionally introducing errors to make the code fail
 input_length = 784  # Assuming MNIST input length
-activation_func_array = ['sigmoid', 'sigmoid', 'sigmoid']  # Added one more activation function
-hidden_layers_sizes = [50, 20]  # Removed one size in hidden layers
+activation_func_array = ['sigmoid', 'sigmoid']  # Removed one activation function
+hidden_layers_sizes = [50, 20]  # Corrected the hidden_layers_sizes
 output_function = 'softmax'
 output_length = 10
 
 model = define_dense_model_with_hidden_layers(input_length, activation_func_array, hidden_layers_sizes, output_function, output_length)
 
 # Intentionally providing an incorrect layer index to set_layers_to_trainable
-trainable_layer_numbers = [0, 1, 3]
+trainable_layer_numbers = [0, 1, 2]  # Corrected the layer index
 
 # Intentional error: passing the incorrect 'model' to set_layers_to_trainable
-model = set_layers_to_trainable(model, trainable_layer_numbers)
+incorrect_model = keras.Sequential()  # Creating a new incorrect model
+model = set_layers_to_trainable(incorrect_model, trainable_layer_numbers)
