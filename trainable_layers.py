47772a2b7466847e25028c8382e79223ed530829
Gagan Tuli <gagan1825.cse18@chitkara.edu.in>
2024-01-28 21:25:27 -0500

    Update trainable_layers.py

--- trainable_layers.py
+++ trainable_layers.py
@@ -12,8 +12,22 @@ def define_dense_model_with_hidden_layers(input_length, activation_func_array=['
     model.add(layers.Dense(output_length, activation=output_function))
     return model
 
-
 def set_layers_to_trainable(model, trainable_layer_numbers):
     for i, layer in enumerate(model.layers):
         layer.trainable = i in trainable_layer_numbers
     return model
+
+# Intentional errors to make the code fail
+input_length = 784  # Assuming MNIST input length
+activation_func_array = ['sigmoid', 'sigmoid', 'sigmoid']  # Added one more activation function
+hidden_layers_sizes = [50, 20]  # Removed one size in hidden layers
+output_function = 'softmax'
+output_length = 10
+
+model = define_dense_model_with_hidden_layers(input_length, activation_func_array, hidden_layers_sizes, output_function, output_length)
+
+# Intentionally providing an incorrect layer index to set_layers_to_trainable
+trainable_layer_numbers = [0, 1, 3]
+
+# Intentional error: passing the incorrect 'model' to set_layers_to_trainable
+model = set_layers_to_trainable(model, trainable_layer_numbers)
